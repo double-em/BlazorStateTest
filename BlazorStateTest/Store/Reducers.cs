7ce95ac00113293663ff3c487a069f3939f26906
double-em <8335996+double-em@users.noreply.github.com>
2022-02-22 20:36:20 +0100

    Another helpful comment

---
+++ BlazorStateTest/Store/Reducers.cs
@@ -0,0 +1,15 @@
+﻿using Fluxor;
+
+namespace BlazorStateTest.Store;
+
+public static class Reducers
+{
+    [ReducerMethod]
+    public static CounterState ReduceIncrementCounterAction(CounterState state, IncrementCounterAction action) =>
+        new (state.ClickCount + 1);
+    
+    // To remove unused parameter warning, instead:
+    // [ReducerMethod(typeof(IncrementCounterAction))]
+    // public static CounterState ReduceIncrementCounterAction(CounterState state) =>
+    //     new CounterState(clickCount: state.ClickCount + 1);
+}
